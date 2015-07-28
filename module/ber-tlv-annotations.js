9d01c4e127aa0d8463c11b09e4eb00a069b712a6
Simon Eumes <simon@sim0n.net>
2015-07-28 21:03:51 +0200

    created module package

--- module/ber-tlv-annotations.js
+++ module/ber-tlv-annotations.js
@@ -1,1 +1,15 @@
-//fill
+var AnnotatedTlv = require('../release/js/annotation/AnnotatedTlv');
+var AnnotationProvider = require('../release/js/provider/AnnotationProvider');
+var AnnotationRegistry = require('../release/js/AnnotationRegistry');
+
+var BerTlvAnnotations = {};
+//AnnotatedTlv
+BerTlvAnnotations.IAnnotatedTlv = AnnotatedTlv.IAnnotatedTlv;
+BerTlvAnnotations.IAnnotatedTlvComponent = AnnotatedTlv.IAnnotatedTlvComponent;
+//AnnotationProvider
+BerTlvAnnotations.IAnnotationProvider = AnnotationProvider.IAnnotationProvider;
+//AnnotationRegistry
+BerTlvAnnotations.IAnnotationRegistry = AnnotationRegistry.IAnnotationRegistry;
+BerTlvAnnotations.AnnotationRegistry = AnnotationRegistry.AnnotationRegistry;
+
+module.exports = BerTlvAnnotations;
